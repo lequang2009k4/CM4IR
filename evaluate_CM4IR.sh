0274e32b5b31ee1bc4437c90db1170e6dd86e03c
TomerG711 <tomergarber@gmail.com>
2025-03-08 17:47:08 +0200

    Added CM4IR code

--- evaluate_CM4IR.sh
+++ evaluate_CM4IR.sh
@@ -13,7 +13,7 @@ python main.py --config lsun_bedroom_256.yml --path_y lsun_bedroom --deg deblur_
 # Inpainting (Random, 80%)
 python main.py --config lsun_bedroom_256.yml --path_y lsun_bedroom --deg inpainting \
   --sigma_y 0.025 -i CM4IR_lsun_bedroom_inpainting_random_80_sigma_y_0.025 --iN 150 --gamma 0.2 \
-  --deltas_injection_type 1 --inpainting_mask_path random_80_mask.npy --model_ckpt lsun_bedroom/cd_bedroom256_lpips.pt # --deltas "0.1,0.1,0.8,0.8" --deltas_injection_type 1
+  --inpainting_mask_path random_80_mask.npy --model_ckpt lsun_bedroom/cd_bedroom256_lpips.pt # --deltas "0.1,0.1,0.8,0.8" --deltas_injection_type 1
 
 # Inpainting (Letters)
 python main.py --config lsun_bedroom_256.yml --path_y lsun_bedroom --deg inpainting \
